bb804983896fc75ec916e8536468a61b0b8fe3ce
Amjad Tadmorey <amjadtadmorey@gmail.com>
2026-07-21 18:33:02 +0300

    .

--- src/ui/Accordion.tsx
+++ src/ui/Accordion.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+
 import { useState, useRef, ReactNode } from "react";
 
 interface AccordionProps {
--- src/ui/RelatedProducts.tsx
+++ src/ui/RelatedProducts.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useRef } from 'react';
 import ProductCard from './ProductCard';
 import { RiArrowLeftSLine, RiArrowRightSLine } from '@remixicon/react';
